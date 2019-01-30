4fb48e48f2c5113a4fade6adce2554b2a9b9a8f5
Yhazarus <jumptotheinfinite@gmail.com>
2019-01-30 16:28:31 +0100

    changes

--- app/src/main/java/com/redilexapps/ndrawerbaseasaide/MainActivity.java
+++ app/src/main/java/com/redilexapps/ndrawerbaseasaide/MainActivity.java
@@ -1,20 +1,26 @@
 package com.redilexapps.ndrawerbaseasaide;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import static com.redilexapps.ndrawerbaseasaide.R.id.drawer_layout;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, fragment_gallery.OnFragmentInteractionListener,
+        fragment_slideshow.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +38,7 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = (DrawerLayout) findViewById(drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
@@ -44,7 +50,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = (DrawerLayout) findViewById(drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -79,23 +85,45 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        boolean FragmentTransaction = false;
+        Fragment fragment = null;
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
+        if (id == R.id.nav_gallery) {
+            // Handle the gallery action
+            fragment = new fragment_gallery();
+            FragmentTransaction = true;
         } else if (id == R.id.nav_slideshow) {
-
+            // Handle the slideshow action
+            fragment = new fragment_slideshow();
+            FragmentTransaction = true;
+        } else if (id == R.id.nav_camera) {
+            Log.i("NavigationDrawer", "entro en opcion camera");
         } else if (id == R.id.nav_manage) {
+            Log.i("NavigationDrawer","entro en opcion manage");
 
         } else if (id == R.id.nav_share) {
+            Log.i("NavigationDrawer","entro en opcion share");
 
         } else if (id == R.id.nav_send) {
+            Log.i("NavigationDrawer","entro en opcion send");
 
         }
+        if (FragmentTransaction){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.content_main, fragment)
+                    .commit();
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            item.setChecked(true);
+            getSupportActionBar().setTitle(item.getTitle());
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
